2eb2df458d79a955af3005aa858004f6d55178eb
mcmaeme <31472623+mcmaeme@users.noreply.github.com>
2020-10-05 22:32:10 +0900

    Update contact.php

--- contact.php
+++ contact.php
@@ -21,10 +21,10 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 	
 // Create the email and send the message
 $to = 'janey326@naver.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "둥지언니 랜선소개팅 지원:  $name $age";
+$email_subject = "둥지언니 랜선소개팅 지원: $name $age";
 $email_body = "둥지언니 랜선소개팅 지원\n\n"."Here are the details:\n\n이름: $name\n\n나이: $age\n\n 이메일: $email_address\n\n사는 곳: $phone\n\n본인소개:\n$message\n\n이상형:$message\n\n";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: $email_address";
 $headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
+$sendmail=mail($to,$email_subject,$email_body,$headers);
 return true;			
 ?>
